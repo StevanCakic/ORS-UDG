131f30842f6be0a445abc810d3574748dc8c9cb5
StevanCakic <stevancakic93@gmail.com>
2020-02-24 11:55:17 +0100

    fix

---
+++ Nedelja 4/dictionaries.py
@@ -0,0 +1,11 @@
+'''
+    Napisati funkciju koja omogucava korisniku da unese
+    n studenata (n parametar funkcije).
+    Svaki student se predstavlja sa kao dictionary oblika:
+    {ime, prezime, godina_studija, prosjek}
+
+    Napisati funkciju koja izdvaja one studenti ciji je prosjek veci od zadatog.
+    Prosjek se zadaje kao parametar funkcije.
+
+    Funkcije testirati
+'''
