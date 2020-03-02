ceff76c040b13ee7eb2b485a45a185d83b1007fc
StevanCakic <stevancakic93@gmail.com>
2020-03-02 14:28:48 +0100

    new code attached

--- Nedelja 5/files.py
+++ Nedelja 5/files.py
@@ -1,8 +1,50 @@
 # Files
 
+f = open("test.txt")
+'''
+print(f.read())
+print(f.read())
+
+f.seek(0)
+print(f.read())
+'''
+f.seek(0)
+for line in f:
+    print(line)
+
+f.seek(0)
+lines = f.readlines()
+for line in lines:
+    print(line)
+
+f.seek(0)
+lines_without_newline = f.read().split("\n")
+for line in lines_without_newline:
+    print(line)
+
+f.close()
+
+with open("test.txt") as f:
+    print(f.read())
+
+with open("novi_fajl.txt", mode="w") as file:
+    file.write("Upis u fajl iz Pythona")
+
 # Zadatak 1
 # Iz fajlova brojevi.txt izdvojiti sve dvocifrene i trocifrene brojeve
 # i upisati ih u novi fajl odabrani_brojevi.txt
+
+nova_lista = []
+with open("brojevi.txt") as file:
+    brojevi = file.read().split("\n")
+    for broj in brojevi:
+        if 1 < len(broj) < 4:
+            nova_lista.append(broj)
+
+print(nova_lista)
+with open("odabrani_brojevi.txt", mode="w") as file:
+    for broj in nova_lista:
+        file.write(broj + "\n")
 
 # Zadatak 2
 # U fajlu studenti nalazi se lista studenata sa ocjenama za zadati predmet.
