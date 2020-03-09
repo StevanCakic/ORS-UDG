c6d3739f49602697e8e15951606590300abb7930
StevanCakic <stevancakic93@gmail.com>
2020-03-09 13:47:15 +0100

    zadaci sa matricama dodati

--- Nedelja 6/files_p2.py
+++ Nedelja 6/files_p2.py
@@ -5,7 +5,34 @@
 
 # Napisati funkciju koja vraca prosjecnu ocjenu ostvarenu na ispitu.
 # E [6 - 6.5), D [6.5, 7.5), C [7.5, 8.5), B [8.5, 9.5), A [9.5, 10]
-# Studente koji su dobili ocjenu F ne ukljucivati u prosjek
+# Studente koji su dobili ocjenu 5 ne ukljucivati u prosjek
+'''
+def izracunaj_prosjek():
+    suma = 0
+    broj_studenata_koji_polozili = 0
+    with open("studenti.txt") as fajl:
+        studenti = fajl.read().split("\n")
+        print(studenti)
+        for student in studenti:
+            podaci_o_studentu = student.split(" ")
+            if int(podaci_o_studentu[2]) > 5:
+                suma = suma + int(podaci_o_studentu[2])
+                broj_studenata_koji_polozili += 1
+
+    prosjek = suma / broj_studenata_koji_polozili
+    if 6 <= prosjek < 6.5:
+        return "E"
+    elif 6.5 <= prosjek < 7.5:
+        return "D"
+    elif 7.5 <= prosjek < 8.5:
+        return "C"
+    elif 8.5 <= prosjek < 9.5:
+        return "B"
+    else: 
+        return "A"
+
+print(izracunaj_prosjek())
+'''
 
 # Zadatak 3
 '''
@@ -22,25 +49,45 @@ Naziv, Opis, Godina, Kolicina, Cijena. Pretpostaviti da su unosi ispravni
 (ne treba raditi validaciju)
 Primjer:
 Input:
-[{“naziv”: ”Televizor”, “opis”: ”LG televizor 43inc”, “godina”: 2019,
-“kolicina”: 10, “cijena”: 300}, {“naziv”: ”Televizor”, “opis”:” Samsung
-televizor 39inc”, “godina”: 2017, “kolicina”: 5, “cijena”: 250}]
+[{"naziv": "Televizor", "opis": "LG televizor 43inc", "godina": 2019,
+"kolicina": 10, "cijena": 300}, {"naziv": "Televizor", "opis":" Samsung
+televizor 39inc", "godina": 2017, "kolicina": 5, "cijena": 250}]
 Output: (products.txt):
 Naziv, Opis, Godina, Kolicina, Cijena
 Televizor, LG televizor 43inc, 2019, 10, 300
 Televizor, Samsung televizor 39inc, 2017, 5, 250
 
+'''
+with open("proizvodi.txt", mode="w") as fajl:
+    lista_proizvoda = [{"naziv": "Televizor", "opis": "LG televizor 43inc", "godina": 2019,
+    "kolicina": 10, "cijena": 300}, {"naziv": "Televizor", "opis":" Samsung televizor 39inc",
+    "godina": 2017, "kolicina": 5, "cijena": 250}]
+    fajl.write("Naziv, Opis, Godina, Kolicina, Cijena\n")
+    for proizvod in lista_proizvoda:
+        fajl.write(f"{proizvod['naziv']}, {proizvod['opis']}, {proizvod['godina']}, {proizvod['kolicina']}, {proizvod['cijena']}\n")    
+'''
 b) Kreirati funkciju get_products_older_than koja ima jedan parametar i
 to godinu koja predstavlja proizvode novije od zadate godine 
 koje želite da izdvojite iz fajla.
 Input: 2018 (izdvoji sve proizvode iz fajla products.txt čija je
-vrijednost ključa “godina” veća ili jednaka godini 2018)
+vrijednost ključa "godina" veća ili jednaka godini 2018)
 Output (ako posmatrate fajl products.txt iz primjera pod a):
 Televizor, LG televizor 43inc, 2019, 10, 300
-
+'''
+with open("proizvodi.txt") as fajl:
+    godina = 2018
+    proizvodi = fajl.read().split("\n")
+    for proizvod in proizvodi:
+        proizvod_info = proizvod.split(", ")
+        try:
+            if int(proizvod_info[2]) >= godina:
+                print(proizvod_info)
+        except:
+            pass
+'''
 c) Kreirati funkciju max_possible_revenue koja računa koliki je
 maksimalni prihod ako se proda svaki proizvod (posmatrati fajl
 products.txt)
-Input: Svi proizvodi učitani kao lista dictionary-a iz fajla products.csv
+Input: Svi proizvodi učitani kao lista dictionary-a iz fajla products.txt
 Output: 4250 (10 * 300 + 5 * 250)
 '''
--- Nedelja 6/matrix.py
+++ Nedelja 6/matrix.py
@@ -1,9 +1,60 @@
 # Rad sa matricama
 
+matrica = [[1,2,3], [4,5,6], [7,8,9]]
+print(matrica)
+for row in matrica:
+    print(row)
+print(matrica[1][1])
+
+num_of_rows = len(matrica) # 3
+num_of_columns = len(matrica[0]) # 3
+
+for i in range(num_of_rows):
+    for j in range(num_of_columns):
+        if matrica[i][j] % 2 == 0:
+            matrica[i][j] = matrica[i][j] ** 2
+        else:
+            matrica[i][j] *= (-1)
+print(matrica)
+
+zbir = 0
+# Kreirati kvadratnu matricu i naci zbir elemenata sa glavne dijagonale
+for i in range(num_of_rows):
+    for j in range(num_of_columns):
+        if i == j:
+            zbir += matrica[i][j]
+
+# Sabrati dvije matrice
+matrica_1 = [[1,3], [2,4]]
+matrica_2 = [[5,6], [7,2]]
+matrica_3 = [[0,0], [0,0]]
+
+num_of_rows = len(matrica_1)
+num_of_columns = len(matrica_1[0])
+
+for i in range(num_of_rows):
+    for j in range(num_of_columns):
+        matrica_3[i][j] = matrica_1[i][j] + matrica_2[i][j]
+print(matrica_3)
+
 matrix = [[0 for i in range(0, 3)] for j in range(0, 3)]
 
-# Kreirati kvadratnu matricu 3x3 i naci zbir svih elemenata
+matrix2 = [[0]*3 for i in range(3)]
 
-# Kreirati kvadratnu matricu i naci zbir elemenata sa glavne dijagonale
 
-# 
+matrix3 = []
+for i in range(3):
+    matrix3.append([0] * 3)
+
+matrix4 = []
+broj_vrsta = int(input("Unesite broj vrsta:"))
+broj_kolona = int(input("Unesite broj kolona:"))
+for i in range(broj_vrsta):
+    matrix4.append([0] * broj_kolona)
+
+for i in range(broj_vrsta):
+    for j in range(broj_kolona):
+        element = int(input(f"Unesite element na poziciji [{i+1},{j+1}]:"))
+        matrix4[i][j] = element
+
+print(matrix4)
